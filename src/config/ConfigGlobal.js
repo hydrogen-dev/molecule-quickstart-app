6216f0b19df35926bddd9bf6be0b5b5ec4444d9b
Matthew Kane <matt@hydrogenplatform.com>
2020-03-29 13:00:04 -0400

    Update ConfigGlobal.js

--- src/config/ConfigGlobal.js
+++ src/config/ConfigGlobal.js
@@ -1,6 +1,6 @@
-export const BASE_URL = 'https://dev.hydrogenplatform.com/';
-export const MOLECULE_URL = 'https://dev.hydrogenplatform.com/molecule/v1/';
-export const CLIENT_ID = 'IntegrationTest';
-export const CLIENT_SECRET = 'IntegrationTest';
+export const BASE_URL = 'https://sandbox.hydrogenplatform.com/';
+export const MOLECULE_URL = 'https://sandbox.hydrogenplatform.com/molecule/v1/';
+export const CLIENT_ID = '';
+export const CLIENT_SECRET = '';
 export const ACCOUNT_TYPE_ID = 'ok';
 export const NETWORK = 'ropsten';
